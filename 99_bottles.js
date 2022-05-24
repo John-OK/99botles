421439d11f5a22983543af5b94ce49ec5abf113f
John OKeeffe <john.ok.esp@gmail.com>
2022-05-24 07:30:43 -0500

    First try

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,11 @@
-function bottleSong() {
-  // Write your code here!
+function bottleSong(num = 99) {
+  for (num; num > 0; num--) {
+    if (num === 1) {
+      console.log("Take one down and pass it around, 1 bottle of beer on the wall.\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.")
+    } else {
+      console.log(`${num} bottles of beer on the wall, ${num} bottles of beer.\nTake one down and pass it around, ${num - 1} bottles of beer on the wall.`)
+    }
+  }
 };
 
 bottleSong();
