9ebc90f543abcffebc6d61a0db638f735ea6ab50
John OKeeffe <john.ok.esp@gmail.com>
2022-05-29 22:36:21 -0500

    Completed JS version. Added Python version.

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,11 @@
-def bottle_song():
-	# write your code here!
+def bottle_song(num=99):
+	if num == 1:
+		print("1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.")
+	for i in range(num, 1, -1):
+		if i == 2:
+			print("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.")
+		else:
+			print(str(i) + " bottles of beer on the wall, " + str(i) + " bottles of beer.\nTake one down and pass it around, " + str(i - 1) + " bottles of beer on the wall.")
 
 
 bottle_song()
